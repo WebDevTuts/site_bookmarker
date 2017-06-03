51deb22f220abfbdd1e0b4e59e677a6fa5c65198
Edwin Chen <edwinchen85@gmail.com>
2017-06-03 21:07:25 +0800

    Prevent form submit default behavior

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 // Listen for form submit
 document.getElementById("myForm").addEventListener("submit", saveBookmark);
 
-function saveBookmark() {
+function saveBookmark(e) {
+  // Prevent form from submitting
+  e.preventDefault();
   console.log("It works!");
 }
