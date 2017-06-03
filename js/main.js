0ca148452da57e9a44455aa567e55bef51c149d4
Edwin Chen <edwinchen85@gmail.com>
2017-06-03 21:41:46 +0800

    Loop over bookmarks array to build up output by adding bookmark title and link

--- js/main.js
+++ js/main.js
@@ -51,4 +51,14 @@ function fetchBookmarks() {
 
   // Build output
   bookmarksResults.innerHTML = '';
+
+  for (var i = 0; i < bookmarks.length; i++) {
+    var name = bookmarks[i].name;
+    var url = bookmarks[i].url;
+
+    bookmarksResults.innerHTML += '<div class="well"><h3>' +
+                                   name +
+                                  ' <a class="btn btn-default" target="_blank" href="'+ url +'">Visit</a>' +
+                                  '</h3></div>';
+  }
 }
